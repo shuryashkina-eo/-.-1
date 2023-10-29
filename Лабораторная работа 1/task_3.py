4483cf290772432ddbdba8012f196349d630a8c7
shuryashkina-eo <149330908+shuryashkina-eo@users.noreply.github.com>
2023-10-29 23:01:15 +0300

    Add files via upload

---
+++ Лабораторная работа 1/task_3.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+first = list_players [:3]
+second = list_players [3:]
+print (first)
+print (second)
+# TODO Разделите участников на две команды
