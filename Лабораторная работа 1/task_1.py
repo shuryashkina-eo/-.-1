254e48f5ca5049aec6e02d2263eaecdd3a5e9a45
shuryashkina-eo <149330908+shuryashkina-eo@users.noreply.github.com>
2023-12-12 12:13:42 +0300

    Update task_1.py

--- Лабораторная работа 1/task_1.py
+++ Лабораторная работа 1/task_1.py
@@ -1,11 +1,12 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-first = (numbers[:4])
-second = (numbers[5:])
+void = numbers.index(None)
+first = (numbers[:void])
+second = (numbers[void + 1:])
 summ_first = sum(first)
 summ_second = sum(second)
 
 middle = (summ_first + summ_second) / len(numbers)
-numbers[4] = middle
+numbers[void] = middle
 print("Измененный список:", numbers )
