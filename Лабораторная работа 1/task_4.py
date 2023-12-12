226737b88cb4999a7c43d7d08180097f864bf2cd
shuryashkina-eo <149330908+shuryashkina-eo@users.noreply.github.com>
2023-12-12 12:14:57 +0300

    Update task_4.py

--- Лабораторная работа 1/task_4.py
+++ Лабораторная работа 1/task_4.py
@@ -1,7 +1,8 @@
 users = ['user1', 'user2', 'user3', 'user1', 'user4', 'user2']
 dictionary = {'Общее количество': 0, 'Уникальные посещения': 0}
 dictionary ['Общее количество'] = len(users)
-unique_users = {'user1': 2, 'user2':2,  'user3': 1, 'user4': 1}
+unique_users = dict.fromkeys(users)
 dictionary ['Уникальные посещения'] = len(unique_users)
 print(dictionary)
 # TODO Добавьте словарь и замените в нем нулевые значения статисчикой посещений
+
